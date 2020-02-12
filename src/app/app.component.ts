e9ed75449fcc43203d83d2d58976f6bec530ae0e
Manduo Dong <madong@madong-mac.local>
2020-02-11 23:22:22 -0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, ValidatorFn, ValidationErrors, FormBuilder } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export const passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+  if (formGroup.get('passFormControl').value === formGroup.get('confirmFormControl').value)
+    return null;
+  else
+    return {passwordMismatch: true};
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent{
+
+  formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      passFormControl : ['', [Validators.required, Validators.minLength(8),]],
+      confirmFormControl : [Validators.required]
+    }, {validator: passwordMatchValidator});
+
+    this.formGroup.get('confirmFormControl').setValue('')
+  }
+
   title = 'game-link';
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  usernameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
+
+  // passFormControl = new FormControl('', [
+  //   Validators.required,
+  //   Validators.minLength(8),
+  // ]);
+
+  // confirmFormControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  get passFormControl() { return this.formGroup.get('passFormControl'); }
+  get confirmFormControl() { return this.formGroup.get('confirmFormControl'); }
+
+  onPasswordInput() {
+    if (this.formGroup.hasError('passwordMismatch'))
+      this.confirmFormControl.setErrors([{'passwordMismatch': true}]);
+    else
+      this.confirmFormControl.setErrors(null);
+  }
+  showLoginError = false;
+  signUpDone = false;
+
+  matcher = new MyErrorStateMatcher();
+
+  checkDisabled() {
+    return !(this.emailFormControl.valid && this.passFormControl.valid && this.confirmFormControl.valid &&
+      this.passFormControl.value == this.confirmFormControl.value)
+  }
+
+  showError() {
+    this.showLoginError = true
+  }
+
+  doneSignUp(v:boolean) {
+    this.signUpDone = v
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +22,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
+  exports: [MatToolbarModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
